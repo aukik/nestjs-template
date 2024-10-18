ea8c6363fec0f74d9bb7c43e2d2455250305a6c8
aukik <aukikaurnabx@gmail.com>
2024-10-18 23:48:36 +0600

    first migration

--- prisma/migrations/20241018174413_first_migration/migration.sql
+++ prisma/migrations/20241018174413_first_migration/migration.sql
@@ -1,6 +1,8 @@
 -- CreateTable
 CREATE TABLE "Passenger" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
 
